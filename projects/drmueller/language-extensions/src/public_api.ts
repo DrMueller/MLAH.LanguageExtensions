06dcf2822fbe256db1edf9aefef5ad91e4a7d0ec
Matthias Mueller <matthiasm@live.de>
2018-09-02 15:58:05 +0200

    WIP

--- projects/drmueller/language-extensions/src/public_api.ts
+++ projects/drmueller/language-extensions/src/public_api.ts
@@ -1,3 +1,4 @@
 export * from './lib/areas/language-extensions/events';
 export * from './lib/areas/language-extensions/maybes';
 export * from './lib/areas/language-extensions/types';
+export * from './lib/areas/language-extensions/functions';
