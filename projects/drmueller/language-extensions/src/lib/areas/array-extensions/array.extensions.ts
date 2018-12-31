ad757229bd1ee0d2d00b6598c07881c654642ac3
Matthias Mueller <matthiasm@live.de>
2018-12-31 16:57:37 +0100

    WIP

--- projects/drmueller/language-extensions/src/lib/areas/array-extensions/array.extensions.ts
+++ projects/drmueller/language-extensions/src/lib/areas/array-extensions/array.extensions.ts
@@ -8,4 +8,12 @@ export class ArrayExtensions {
     const entriesNotInOtherArray = thisArray.filter(entry => !otherArray.some(otherEntry => callbackFn(entry, otherEntry)));
     return entriesNotInOtherArray;
   }
+
+  public static getItemsInOtherArray<T>(
+    thisArray: T[],
+    otherArray: T[],
+    callbackFn: TwoArgFunc<T, T, boolean>): T[] {
+    const entriesNotInOtherArray = thisArray.filter(entry => otherArray.some(otherEntry => callbackFn(entry, otherEntry)));
+    return entriesNotInOtherArray;
+  }
 }
--- projects/drmueller/language-extensions/src/public_api.ts
+++ projects/drmueller/language-extensions/src/public_api.ts
@@ -1,3 +1,5 @@
 export * from './lib/areas/events';
 export * from './lib/areas/maybes';
 export * from './lib/areas/types';
+export * from './lib/areas/array-extensions';
+export * from './lib/areas/directories';
